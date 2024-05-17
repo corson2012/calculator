ff985e5409994a49ae5fa38a1fdd10edb51018ce
corson2012 <148826531+corson2012@users.noreply.github.com>
2024-05-17 15:36:41 -0500

    added event listeners no number buttons

--- script.js
+++ script.js
@@ -6,3 +6,58 @@ const fiveBtn = document.getElementById("five-btn");
 const sixBtn = document.getElementById("six-btn");
 const sevenBtn = docuemnt.getElementById("seven-btn");
 const eightBtn = document.getElementById("eight-btn");
+const nineBtn = document.getElementById("nine-btn");
+const zeroBtn = document.getElementById("zero-btn");
+const doubleZeroBtn = document.getElementById("double-zero-btn");
+const clearInputBtn = document.getElementById("clear-input-btn");
+const clearAllBtn = document.getElementById("clear-all-btn");
+const percentBtn = document.getElementsById("perecent-btn");
+const divisionBtn = document.getElementById("division-btn");
+const multipyBtn = document.getElementById("multiply-btn");
+const subtractionBtn = document.getElementById("subtraction-btn");
+const additionBtn = document.getElementById("addition-btn");
+const periodBtn = document.getElementById("period-btn");
+const equalBtn = document.getElementById("equal-btn");
+const inputField = document.getElementById("input-field");
+const userInput = document.getElementById("user-input");
+
+const calculateInput = (input) => {
+    let solution 
+
+};
+
+oneBtn.addEventListener("click", () => {
+    userInput.textContent += "1";
+});
+
+twoBtn.addEventListener("click", () => {
+
+});
+
+threeBtn.addEventListener("click", () => {
+
+});
+
+fourBtn.addEventListener("click", () => {
+
+});
+
+fiveBtn.addEventListener("click", () => {
+
+});
+
+sixBtn.addEventListener("click", () => {
+
+});
+
+sevenBtn.addEventListener("click", () => {
+
+});
+
+eightBtn.addEventListener("click", () => {
+
+});
+
+nineBtn.addEventListener("click", () => {
+
+});
